cd7354f19d7a4eae6e0a7ab067e60bf36b14b856
Kumar <amit.kumar02@delta.com>
2022-07-29 14:08:08 +0530

    v4

--- runner.js
+++ runner.js
@@ -1,7 +1,12 @@
 const args = require('minimist')(process.argv);
 const axios = require('axios');
+const percentile = require('percentile');
+const lodash = require('lodash');
+const employess = require('./data');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+console.log("Total employees: " + employess.length);
 
 if (!args.url || !args.waitTime || !args.threadCount || !args.requestPerThread) {
     console.log("node runner url=serviceUrl waitTime=? threadCount=?");
@@ -30,9 +35,12 @@ axios.interceptors.response.use(x => {
     return x;
 })
 
-
+var dataIndex=17500;
 var requestNumber = 1;
 var _iThreadCount = parseInt(args.threadCount);
+var successCount = 0;
+var failureCount = 0;
+var responseTimes = [];
 
 for (var i = 1; i <= _iThreadCount; i++) {
     start();
@@ -41,15 +49,48 @@ for (var i = 1; i <= _iThreadCount; i++) {
 async function start() {
     var _iRequestCount = parseInt(args.requestPerThread);
     for (var n = 0; n < _iRequestCount; n++) {
-        console.log(`Starting request number ${requestNumber++}`);
+        _sURL = args.url.replace("ARGUMENT", employess[dataIndex++]);
+        console.log(`Starting request number ${requestNumber++} with ${_sURL}`);
         axios
-            .get(args.url, { timeout: 900000 })
+            .get(_sURL,
+                {   headers: {
+                    "Accept": "application/json",
+                    "TransactionID": "1",
+                    "applicationOwnerCode": "CNO",
+                    "apiResourceName": "CNO-CMA",
+                    "apiRequestUtcTs": "SYSDATE",
+                    "applicationName": "CNO-Crew Mobile App",
+                    "applicationVersionNum": "1",
+                    "applicationDeviceName": "CNO",
+                    "applicationDeviceOS": "CNO",
+                    "applicationDeviceMacAddress": "CNO",
+                    "networkIpAddress": "CNO",
+                    "apiConsumerRecordText": "CNO",
+                    "employeeId": "0000300300",
+                    "Content-Type": "application/json",
+                    "Cookie": "045e94007b2777fba90c88d1cb5e712b=47afc34ee0605b2837867d66fd29ef55",
+                   "Authorization": "Bearer g4xpvFCPA95x5isK8ouFOidyNb21"
+                },
+                    timeout: 900000 }
+                )
             .then(res => {
+                successCount++;
+                responseTimes.push(res.responseTime);
                 console.log(`Returned statusCode: ${res.status} in ${res.responseTime / 1000} sec`);
             })
             .catch(error => {
+                failureCount++;
                 console.error("Returned error", error);
             });
         await new Promise(r => setTimeout(r, parseInt(args.waitTime) * 1000));
     }
 }
+
+process.on('exit', (code) =>{
+    console.log(`Success: ${successCount}`);
+    console.log(`Failure: ${failureCount}`);
+    console.log(`Minimum response time: ${lodash.min(responseTimes) /1000} sec`);
+    console.log(`Maximum response time: ${lodash.max(responseTimes) /1000} sec`);
+    console.log(`Average response time: ${lodash.mean(responseTimes) /1000} sec`);
+    console.log(`90th P response time: ${percentile(90, responseTimes) /1000} sec`);
+});
